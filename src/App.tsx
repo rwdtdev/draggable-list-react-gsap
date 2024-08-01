123b8720b26f3c2d9d448e6db75ddf254d6d1a67
a.grachev <a.grachev@rwdt.ru>
2024-08-01 17:12:40 +0300

    add useState to manage data, partially works

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { Draggable } from "gsap/Draggable";
+import { nanoid } from "nanoid";
 
 type Sortable = {
   dragger: Draggable;
@@ -15,6 +16,15 @@ gsap.registerPlugin(Draggable);
 gsap.registerPlugin(useGSAP);
 let container: Element | undefined = undefined;
 function App() {
+  const [data, setData] = useState([
+    "1 Alfa",
+    "2 Bravo",
+    "3 Charlie",
+    "4 Delta",
+  ]);
+
+  const actionType = useRef<"add" | "del" | null>(null);
+
   useEffect(() => {
     container = document.querySelector(".g-container")!;
   }, []);
@@ -24,15 +34,16 @@ function App() {
       const listItems = Array.from(document.querySelectorAll(".g-list-item")); // Array of elements
       const sortables = listItems.map(Sortable); // Array of sortables
       const total = sortables.length;
+      // const dataClone = structuredClone(data);
 
       // if (container) gsap.to(container, { autoAlpha: 1 });
 
       function changeIndex(item: Sortable, to: number) {
         // Change position in array
-        // arrayMove(sortables, item.index, to);
         sortables.splice(to, 0, sortables.splice(item.index, 1)[0]);
 
         // Change element's position in DOM. Not always necessary. Just showing how.
+        // dataClone.splice(to, 0, dataClone.splice(item.index, 1)[0]);
         // if (to === total - 1) {
         //   container!.appendChild(item.element);
         // } else {
@@ -53,19 +64,23 @@ function App() {
           force3D: true,
           scale: 1.1,
           paused: true,
+          delay: 0.1,
         });
 
         const dragger = new Draggable(element, {
           onPress: () => {
             animation.play();
           },
-          // onDragStart: () => {
-          //   this.update();
-          // },
+
           onDrag: onDragFunc,
           onRelease: () => {
             animation.reverse();
             gsap.to(element, { y: sortable.index * rowSize });
+            // .then(() => {
+            //   if (actionType.current !== "del") {
+            //     setData(dataClone);
+            //   }
+            // });
           },
           cursor: "inherit",
           type: "y",
@@ -81,6 +96,24 @@ function App() {
 
         gsap.to(element, { y: index * rowSize });
 
+        // const tl = gsap.timeline();
+
+        // const fromIndex =
+        //   index === data.length - 1 && actionType.current === "add"
+        //     ? index - 1
+        //     : index;
+
+        // tl.set(element, { zIndex: -index })
+        //   .fromTo(
+        //     element,
+        //     { y: fromIndex * rowSize },
+        //     {
+        //       y: index * rowSize,
+        //       duration: 1,
+        //     }
+        //   )
+        //   .set(element, { zIndex: 0 });
+
         function setIndex(index: number) {
           sortable.index = index;
           // order!.textContent = String(index + 1);
@@ -91,11 +124,6 @@ function App() {
           }
         }
 
-        // function onDragStartFunc() {
-        //   // animation.play();
-        //   this.update();
-        // }
-
         function onDragFunc() {
           // Calculate the current index based on element's position
           const index = clamp(Math.round(this.y / rowSize), 0, total - 1);
@@ -105,35 +133,37 @@ function App() {
           }
         }
 
-        // function onReleaseFunc() {
-        //   animation.reverse();
-        //   layout();
-        // }
-
-        // function layout() {
-        //   gsap.to(element, { y: sortable.index * rowSize });
-        // }
-
         return sortable;
       }
-
-      // Changes an elements's position in array
-      // function arrayMove(array: Array<Sortable>, from: number, to: number) {
-      //   array.splice(to, 0, array.splice(from, 1)[0]);
-      // }
 
       // Clamps a value to a min/max
       function clamp(value: number, a: number, b: number) {
         return value < a ? a : value > b ? b : value;
       }
+      actionType.current = null;
     },
-    { scope: container }
+    { scope: container, dependencies: [data] }
   );
   return (
     <>
       <h1>List</h1>
-      <section className="g-container">
-        <div className="g-list-item">
+      <ul className="g-container">
+        {data.map((item) => (
+          <li className="g-list-item" key={item}>
+            <div className="item-content">
+              <span
+                onClick={() => {
+                  setData((st) => st.filter((it) => it !== item));
+                  actionType.current = "del";
+                }}
+              >
+                del
+              </span>{" "}
+              {item}
+            </div>
+          </li>
+        ))}
+        {/* <div className="g-list-item">
           <div className="item-content">1 Alpha</div>
         </div>
 
@@ -147,8 +177,16 @@ function App() {
 
         <div className="g-list-item">
           <div className="item-content">4 Delta</div>
-        </div>
-      </section>
+        </div> */}
+      </ul>
+      <button
+        onClick={() => {
+          setData((st) => [...st, nanoid()]);
+          actionType.current = "add";
+        }}
+      >
+        s add item
+      </button>
     </>
   );
 }
